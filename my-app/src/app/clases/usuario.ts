2c12b9e908a4c585bc7adb1799464565eeb0d86d
alumno <alumno@utnfra.com.ar>
2019-03-21 22:09:59 -0300

    21-03

---
+++ my-app/src/app/clases/usuario.ts
@@ -0,0 +1,10 @@
+export class Usuario {
+  public name: string;
+  public pass: string;
+
+  constructor(name: string, pass: string) {
+    this.name = name;
+    this.pass = pass;
+}
+
+}
---
+++ my-app/src/app/componentes/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/clases/usuario';
+
+@Component({
+  selector: 'app-login', // Nuevo tag HTML <app_login><app_login>
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public usuario: Usuario;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
