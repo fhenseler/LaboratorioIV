d08e6ab520cb29575f7960fe835dc0a0bd028b60
alumno <alumno@utnfra.com.ar>
2019-03-21 22:12:01 -0300

    21/03

--- my-app/src/app/componentes/login/login.component.ts
+++ my-app/src/app/componentes/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/clases/usuario';
 
 @Component({
-  selector: 'app-login', // Nuevo tag HTML <app_login><app_login>
+  selector: 'app-login', // Nuevo tag HTML <app_login></app_login>
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
