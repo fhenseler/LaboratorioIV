dceafb56598b27decde8243f89a7bf041c402047
Federico Henseler <fhenseler@outlook.com>
2019-03-28 13:36:03 -0300

    28/3

--- my-app/src/app/clases/usuario.ts
+++ my-app/src/app/clases/usuario.ts
@@ -2,9 +2,7 @@ export class Usuario {
   public name: string;
   public pass: string;
 
-  constructor(name: string, pass: string) {
-    this.name = name;
-    this.pass = pass;
+  constructor() {
 }
 
 }
--- my-app/src/app/componentes/login/login.component.ts
+++ my-app/src/app/componentes/login/login.component.ts
@@ -9,11 +9,20 @@ import { Usuario } from 'src/app/clases/usuario';
 export class LoginComponent implements OnInit {
 
   public usuario: Usuario;
+  public guardado: boolean;
 
   constructor() {
+    this.usuario = new Usuario();
+    this.guardado = false;
   }
 
   ngOnInit() {
+  }
+
+  guardar()
+  {
+    this.guardado = true;
+    console.info("El objeto: ", this.usuario);
   }
 
 }
