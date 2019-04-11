39f3ab4c16d9c75fd88a72829bc2868a4af88e44
Federico Henseler <fhenseler@outlook.com>
2019-04-11 15:55:02 -0300

    11/4

---
+++ TP Sala de Juegos/src/app/componentes/principal/principal.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent implements OnInit {
+ public status: any = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+  // constructor() {  }
+
+  constructor(private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  Juego(tipo: string) {
+    switch (tipo) {
+      case 'Adivina':
+          this.router.navigate(['/Juegos/Adivina']);
+        break;
+      case 'Agilidad':
+          this.router.navigate(['/Juegos/Agilidad']);
+        break;
+      case 'AdivinaMasListado':
+          this.router.navigate(['/Juegos/AdivinaMasListado']);
+        break;
+      case 'AgilidadaMasListado':
+          this.router.navigate(['/Juegos/AgilidadaMasListado']);
+        break;
+    }
+  }
+
+ 
+
+}
